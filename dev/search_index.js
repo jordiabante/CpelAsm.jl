562b04ac65097346cc2f106243a667cc41707449
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2019-10-09 18:26:44 +0000

    build based on 877349e

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#Example.jl-Documentation-1","page":"Example.jl Documentation","title":"Example.jl Documentation","text":"","category":"section"},{"location":"#","page":"Example.jl Documentation","title":"Example.jl Documentation","text":"comp_Z(x)","category":"page"}]
+}
