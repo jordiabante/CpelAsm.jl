d71c7ba8fc2f674ccf75cd7f73222ae7756b9f42
Jordi Abante <jabante1@jhu.edu>
2019-10-09 17:39:54 -0400

    Added other buil tests, and info in docs.

--- docs/make.jl
+++ docs/make.jl
@@ -2,9 +2,11 @@ push!(LOAD_PATH,"../src/")
 using Documenter,CpelAsm
 
 makedocs(sitename = "CpelAsm Documentation",
-         format = Documenter.HTML(prettyurls=get(ENV,"CI",nothing)=="true")
+         format = Documenter.HTML(prettyurls=get(ENV,"CI",nothing)=="true"),
+         authors = "Jordi Abante",
 )
 
 deploydocs(
-    repo = "github.com/jordiabante/CpelAsm.jl.git"
+    repo = "github.com/jordiabante/CpelAsm.jl.git",
+    target = "build",
 )
